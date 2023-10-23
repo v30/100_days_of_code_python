d1a22b3ac095f52744bb09f13e43c1310f06d45d
owen nel <affiliateowen@gmail.com>
2023-10-23 17:55:41 +0200

    Changed the comments

--- day1/main.py
+++ day1/main.py
@@ -1,11 +1,11 @@
-#Python script to generate a band name based on the city you were born in and your pet name
+# Python script to generate a band name based on the city you were born in and your pet name
 
-#Print a welcome message
+# Print a welcome message
 print('Welcome to the Band Name Generator.')
 
-#Get input from the user
+# Get input from the user
 city = input("What's the name of the city you grew up in?")
 pet_name = input("What's your pet's name?")
 
-#Print the result
+# Print the result
 print("Your band name could be "+city+" "+pet_name)
